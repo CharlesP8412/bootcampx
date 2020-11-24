b4b26dcd69002fd56e1f4a1023ef91d2c10d8b99
Charles P <charles.prystupa@gmail.com>
2020-11-24 20:55:01 +0000

    Save Query 13

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,9 +1,10 @@
-
-FROM (SELECT teachers.name AS teacher,
-cohorts.name AS cohort
+SELECT DISTINCT teachers.name AS teacher,
+cohorts.name AS cohort,
+COUNT(teachers.name) as count
 FROM assistance_requests reqs
 JOIN teachers ON teachers.id = reqs.teacher_id
 JOIN students ON students.id = reqs.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teacher)
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
